5971010af1183946bed3bf3fb19cf8d20215a4ab
Boyarchuck81 <>
2023-11-28 01:09:48 +0300

    Test004

---
+++ CSharp Lection/Test004/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//System.Console.WriteLine("Введите число: "); 
+//int userNamber = Convert.ToInt32(Console.ReadLine());
+//int result = userNamber * userNamber;
+//Console.WriteLine(result);
+
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
