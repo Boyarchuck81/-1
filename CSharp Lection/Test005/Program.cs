b95a1fd303bba76c23b9e6b0559f98acdb3cbb54
Boyarchuck81 <>
2023-11-28 01:42:31 +0300

    Test005

---
+++ CSharp Lection/Test005/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя: "); 
+string username = Console.ReadLine();
+
+if(userName == "Маша")
+{
+ Concole.WriteLine("Ура, это же Маша!")   
+}
+else
+{
+Console.Write("Привет, ");
+Concole.WriteLine(username);
+}
