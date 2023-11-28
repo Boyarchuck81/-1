fe182657b88a86152be3ebfdb4f56c6042a86f5d
Boyarchuck81 <>
2023-11-28 23:10:11 +0300

    Изменила главную  папку

---
+++ CSharp_HomeWork 02/Zadata01/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ Напишите программу, которая принимает на вход число и проверяет, 
+ кратно ли оно одновременно 7 и 23.
+a=50 => нет
+a=7 => нет
+a=322 => да
+*/
+
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA % 7 == 0 && numberA % 23== 0)
+{
+    System.Console.WriteLine("да");
+}
+else
+{
+    System.Console.WriteLine("нет");
+}
---
+++ CSharp_HomeWork 02/Zadata02/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0
+и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+x=2; y=3 -> 1 
+x=-5; y=3 -> 2
+x=-3; y=-2 -> 3
+x=4; y=-2 -> 4
+*/ 
+
+Console.WriteLine("Введите координаты точки. X: ");
+int userX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки. Y: ");
+int userY = Convert.ToInt32(Console.ReadLine());
+if (userX>0 && userY>0) System.Console.WriteLine("Точка находится в первой четверти");
+if (userX<0 && userY>0) System.Console.WriteLine("Точка находится во второй четверти");
+if (userX<0 && userY<0) System.Console.WriteLine("Точка находится в третьей четверти");
+if (userX>0 && userY<0) System.Console.WriteLine("Точка находится в четвертой четверти");
+if (userX==0 || userY==0) System.Console.WriteLine("Точка не находится ни на одной координатной четверти");
