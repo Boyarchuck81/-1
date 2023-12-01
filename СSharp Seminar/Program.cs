0742f4773fb3a8172e94de6f103ea46f669527d3
Boyarchuck81 <>
2023-12-01 21:43:44 +0300

    1

--- СSharp Seminar/Program.cs
+++ СSharp Seminar/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿// Напишите программу, которая принимает на вход число и выдает его квадрат/
+System.Console.WriteLine("Введите число: "); //cw -бытрый ввызов этой команды
+int userNamber = Convert.ToInt32(Console.ReadLine());
+int result = userNamber * userNamber;
+System.Console.WriteLine(result);
+
+// System.Console.WriteLine("Введите число: "); //cw быстрый вызов Console.WriteLine
+// int userNamber = Convert.ToInt32(Console.ReadLine());
+// int result = userNamber * userNamber;
+// Console.WriteLine(result);
