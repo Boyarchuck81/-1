b356fa9fb27d787d0f93d6389bc8e48f5f596518
Boyarchuck81 <>
2023-12-03 00:44:31 +0300

    идеальное решение

--- CSharp_HomeWork 02/Zadata01/Program.cs
+++ CSharp_HomeWork 02/Zadata01/Program.cs
@@ -17,3 +17,22 @@ else
 {
     System.Console.WriteLine("нет");
 }
+
+//Идеальное решение
+using System;
+class Program
+{
+static void Main()
+{
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 7 == 0 && a % 23 == 0)
+{
+Console.WriteLine("да");
+}
+else
+{
+Console.WriteLine("нет");
+}
+}
+}
