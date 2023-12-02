e898f3d352e2161ac054754cbda1e30514054002
Boyarchuck81 <>
2023-12-03 00:49:53 +0300

    идеальное решение3

--- CSharp_HomeWork 02/Zadata03/Program.cs
+++ CSharp_HomeWork 02/Zadata03/Program.cs
@@ -20,3 +20,17 @@ else
 {
      System.Console.WriteLine("Наибольшее " + last);
 }
+
+//идеальное решение
+using System;
+class Program
+{
+static void Main()
+{
+Console.Write("Введите число из отрезка [10, 99]: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;Console.WriteLine(maxDigit);
+}
+}
