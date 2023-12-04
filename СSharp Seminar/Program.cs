aafc488d86a5398efa5fa1a6a018f8a37f450919
Boyarchuck81 <>
2023-12-05 01:30:21 +0300

    семинар 5

--- СSharp Seminar/Program.cs
+++ СSharp Seminar/Program.cs
@@ -849,3 +849,58 @@ a = 891 => 81
 // PrintMatrix(matrix);
 // Console.WriteLine($"Среднее арифметическое строк: [{string.Join(", ", AVG(matrix))}]");
 
+// Семинар 5
+
+// Задание 1.
+Console.Clear();
+// char[] chars = {'a', 'b', 'c', 'd'};
+// Console.Write("Введите символы через пробел: ");
+// char[] chars = Console.ReadLine()!.Split().Select(x => char.Parse(x)).ToArray();
+// /*
+// 1. "a b 1 2 3 4 6 c d e"
+// 2. ["a", "b", "1", "2", "3", "4", "6", "c", "d", "e"]
+// 3. 'a', 'b', '1', '2', '3', '4', '6', 'c', 'd', 'e'
+// 4. ['a', 'b', '1', '2', '3', '4', '6', 'c', 'd', 'e']
+// */
+// string result = string.Empty; // создание пустой строки
+// // string result = "";
+// foreach (char element in chars){
+//     result = result + element;
+// }
+// Console.WriteLine(result);
+
+
+
+// Console.Clear();
+// Console.Write("Введите строку: ");
+// string initial = Console.ReadLine()!;
+// char[] chars = new char[initial.Length];
+// // Hello
+// // 01234
+// for (int i = 0; i < initial.Length; i++){
+//     chars[i] = initial[i];
+// }
+// Console.WriteLine($"Результат: [{string.Join(" ", chars)}]");
+
+
+// bool is_vowels(char el, string vowels){
+//     for (int i = 0; i < vowels.Length; i++){
+//         if (vowels[i] == el)
+//             return true;
+//     }
+//     return false;
+// }
+
+    
+// Console.Clear();
+// Console.Write("Введите строку: ");
+// string initial = Console.ReadLine()!;
+// string vowels = "aeuioy";
+// int count = 0;
+// for (int i = 0; i < initial.Length; i++){
+//     if (is_vowels(initial[i], vowels))
+//         count++;
+// }
+// Console.WriteLine(count);
+
+// https://learn.microsoft.com/ru-ru/dotnet/api/system.char.isupper?view=netframework-4.8
