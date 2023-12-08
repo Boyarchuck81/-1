da6826e151e785d987a5926fbe3a475fab63bd47
Boyarchuck81 <>
2023-12-09 00:18:35 +0300

    лекция

--- CSharp Lection/Test002 Username/Program.cs
+++ CSharp Lection/Test002 Username/Program.cs
@@ -1,5 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите Ваше имя: ");
-string username = Console.ReadLine();
-Console.Write("Привет,");
-Console.Write(username);
+// Console.Write("Введите Ваше имя: ");
+// string username = Console.ReadLine()!;
+// Console.Write($"Привет,\n{username}!");
+
+// Домашка
+int a = 5;
+int b = 4;
+int c = 1;
+int d = 2;
+int e = a + b;
+int f = c + d;
+double result = e / f;
+System.Console.WriteLine(result);
--- СSharp Seminar/Program.cs
+++ СSharp Seminar/Program.cs
@@ -124,6 +124,47 @@ System.Console.WriteLine(result);
 //     System.Console.Write(i + "\t");
 // }
 
+/*
+ ЗадачаНапишите программу, которая выводит случайное число из отрезка [10, 99]
+  и показывает наибольшую цифру числа.
+78 -> 8
+12-> 2
+85 -> 8
+*/ 
+int number = new Random().Next(10,100);
+System.Console.WriteLine(number);
+int first = number / 10;
+int last = number % 10;
+if (first>last)
+{
+    System.Console.WriteLine("Наибольшая " + first);
+}
+else
+{
+     System.Console.WriteLine("Наибольшее " + last);
+}
+
+/*
+16.Напишите программу, которая принимает на вход два числа и проверяет, 
+является ли одно число квадратом другого.
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8,9 -> нет
+*/
+System.Console.WriteLine("Введите число: ");
+int namberA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число: ");
+int namberB = Convert.ToInt32(Console.ReadLine());
+if (namberA * namberA == namberB || namberB * namberB == namberA)
+{
+    System.Console.WriteLine("да");
+}
+else
+{
+    System.Console.WriteLine("нет");
+}
+
 // Семинар 1
 
 /*
