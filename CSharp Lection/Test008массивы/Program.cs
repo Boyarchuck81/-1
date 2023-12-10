73f0170f43575480b762a82627d36978284e2f10
Boyarchuck81 <>
2023-12-10 14:45:00 +0300

    test009

---
+++ CSharp Lection/Test008массивы/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// int[] arr = new int[5];
+// arr [0] = 5;
+// arr [1] = 7;
+// arr [2] = 3;
+// arr [3] = 8;
+// arr [4] = 1;
+// Console.WriteLine(arr[0]);
+// Console.WriteLine(arr[1]);
+// Console.WriteLine(arr[2]);
+// Console.WriteLine(arr[3]);
+// Console.WriteLine(arr[4]);
+
+// или
+
+int [] array = {5,7,3,8,1};     //упрощенный формат
+System.Console.WriteLine(array[]);
+// array = {1,2,3,4,5}           //массив в Python
