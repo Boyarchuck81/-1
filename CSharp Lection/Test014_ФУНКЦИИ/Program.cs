428ba1586d76dec46ca95baf6cacbc8fc173e86d
Boyarchuck81 <>
2023-12-10 20:55:33 +0300

    test_14

---
+++ CSharp Lection/Test014_ФУНКЦИИ/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+void PrintSquares(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+      System.Console.Write($"{i*i} ");
+      i++;
+    }
+}
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
