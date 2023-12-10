ed85fcaf331dc5b214a855f5448bb19ba6f28cd0
Boyarchuck81 <>
2023-12-10 15:38:42 +0300

    test011

---
+++ CSharp Lection/Test010четность/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int N = 10;
+int[] arr = {1,2,3,4,5,6,7,8,9,10};
+int i = 0;
+while(i<N)                                //while(i < arr.Length)
+{
+    arr[i] = i + 1;
+    if(arr[i] % 2 == 0)
+    System.Console.Write($"{arr[i]} ");
+    i = i + 1;     //i++
+}
---
+++ CSharp Lection/Test011 max_гиря/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int N = 10;
+int[] arr = {1,8,15,9,14,5,1,13,7,2};
+int i = 0;
+int max = arr[0];
+while(i<N)                                //while(i < arr.Length)
+{
+    
+    if(arr[i] > max){
+    max = arr[i];}
+    i++;     
+}
+System.Console.Write(max);
