d09fdad4d9f16a9667d4f52954d7a43e6657b125
Boyarchuck81 <>
2023-12-10 17:32:34 +0300

    test013

---
+++ CSharp Lection/Test013foreach/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// int N = 10;
+int[] arr = {1,8,15,9,14,5,1,13,7,2};
+int max = arr[0];
+foreach(int element in arr)                             
+{
+    if(element > max){
+    max = element;} 
+}
+System.Console.WriteLine(max);
+                     
