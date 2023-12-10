9aa6bd1407d74fa0af7bc4be2a2c28015c165fbd
Boyarchuck81 <>
2023-12-10 17:34:56 +0300

    test_11,12,13

---
+++ CSharp Lection/Test009от1доN/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int N = 10;
+int[] arr = new int[N];
+int i = 0;
+while(i<N)                                //while(i < arr.Length)
+{
+    arr[i] = i + 1;
+    System.Console.Write($"{arr[i]} ");
+    i = i + 1;     //i++
+}
---
+++ CSharp Lection/Test012_for/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// int[] arr = {1,2,3,4,5};
+// for(int i = 0; i < arr.Length; i = i + 1)                          //вывод массива от 1 до 5     
+// {
+//     System.Console.Write($"{arr[i]} ");
+// }
+
+// int n = 100;
+// int sum = 0;
+
+//  for(int i = 1; i < n; i = i + 1){
+//     sum = sum + 1;
+//  }
+//  System.Console.Write(sum);                      //не получилось, выводит 99
+
+int N = 10;
+int[] arr = {1,8,15,9,14,5,1,13,7,2};
+int max = arr[0];
+for(int i = 1; i < N; i++)                             
+{
+    if(arr[i] > max){
+    max = arr[i];} 
+}
+System.Console.WriteLine(max);
