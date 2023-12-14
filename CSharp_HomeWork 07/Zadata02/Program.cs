76397f1af08e9b7a799e2d3b645b353a515c526e
Boyarchuck81 <>
2023-12-14 05:28:27 +0300

    домашка 7

---
+++ CSharp_HomeWork 07/Zadata02/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// Console.Clear();
+System.Console.WriteLine("Введите число m: ");
+int mNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число n: ");
+int nNumber = Convert.ToInt32(Console.ReadLine());
+int Ackerman(int mNum, int nNum)
+{
+    if (mNum == 0)
+    {
+        return nNum + 1;
+    }
+    else if (mNum != 0 && nNum == 0)
+    {
+        return Ackerman(mNum - 1, 1);
+    }
+    else
+    {
+        return Ackerman(mNum - 1, Ackerman(mNum, nNum - 1));
+    }
+}
+int result = Ackerman(mNumber, nNumber);
+System.Console.WriteLine(result);
---
+++ CSharp_HomeWork 07/Zadata03/Program.cs
@@ -0,0 +1,21 @@
+﻿// /Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+ using static System.Console;
+int[] numbers = { -33, 28, 5, -15, 63, 87, 43 };
+
+void ReverseArray(int[] array, int Element1, int Element2)
+{
+    if (Element1 < Element2)
+{
+int arr = array[Element1];
+array[Element1] = array[Element2];
+array[Element2] = arr;
+ReverseArray(array, Element1 + 1, Element2 - 1);
+}
+}
+ReverseArray(numbers, 0, numbers.Length - 1);
+
+foreach (int i in numbers)
+{
+    System.Console.Write($"{i}  ");
+}
